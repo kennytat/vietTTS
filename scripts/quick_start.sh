d07b9cb62271191547ee0f7bd753a30161e7119b
kennytat <82075643+kennytat@users.noreply.github.com>
2022-08-27 23:20:37 +0700

    Update quick_start.sh

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -3,9 +3,9 @@ if [ ! -f $HIFIGAN_CHECKPOINT ]; then
   pip3 install gdown
   echo "Downloading models..."
   mkdir -p -p assets/infore/{nat,hifigan}
-  gdown 1-3OgKYHf77n0pxX0GDsfNlL9yLAwx61l -O assets/infore/nat/duration_latest_ckpt.pickle
-  gdown 1-S6_fsLCc0JqROJgjfHa4cxC5BivvJEy -O assets/infore/nat/acoustic_latest_ckpt.pickle
-  gdown 1ZrzXlBa2ugGGaPcE8DZA98hTBSiBNmJu -O $HIFIGAN_CHECKPOINT
+  gdown 1zjB02X0dVb6FkFZetJA0yyFyGoege9mf -O assets/infore/nat/duration_latest_ckpt.pickle
+  gdown 1mVdJQP6F7CIiNUbqzA8X4aPCj_43xVVR -O assets/infore/nat/acoustic_latest_ckpt.pickle
+  gdown 1Ry2dM8rF7Wsi_B21YCNzLUWUlOCzOx24 -O $HIFIGAN_CHECKPOINT
   python -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=$HIFIGAN_CHECKPOINT
 fi
 
